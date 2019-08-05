043b98fc43f823426b84c4938d957c2c4e59edf1
xinyi <xinyi2017mm@gmail.com>
2019-08-05 16:41:03 -0700

    output the day of buy the stock and the day of sell the stock and recompile it

--- StockBuySell.java
+++ StockBuySell.java
@@ -9,21 +9,30 @@ public class StockBuySell {
         int[] Maxdp=new int[prices.length];
         
         Mindp[0]=prices[0];
+	int buyday, sellday;
+	buyday = 0;
+	sellday = 0;
         
         for(int i=1;i<prices.length;i++){
 //maintain the lowest prices of the stock as a buying price.
+	   if (Mindp[i - 1] > prices[i])
+		buyday = i;
             Mindp[i]=Math.min(Mindp[i-1],prices[i]);
 			
         //    Mindp[i-1]=Mindp[i];
         //    System.out.println("No "+i+" min buying price "+Mindp[i]);
 //current benefit we can get if we selling at the i day.
             Maxdp[i]=prices[i]-Mindp[i];
-//update the global max profit;            
+//update the global max profit;
+	   if (Maxdp[i] > reg)
+		sellday = i;            
             reg=Math.max(reg,Maxdp[i]);
-            
         }
+	System.out.println("the buy day is " + (buyday + 1));
+	System.out.println("the sell day is " + (sellday + 1));
         return reg;
     }
+	
 	public static void main(String args[])
 	{
 		int[] arr = {3, 8, 8, 55, 38, 1, 7, 42, 54, 53};
