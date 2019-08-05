ac98be60868904a472a6a5a381abe3d2e72c0501
XxZhang2017 <mynewvegaan@gmail.com>
2019-08-05 16:26:22 -0700

    Update StockBuySell.java
    Add the commend to explain the code.

--- StockBuySell.java
+++ StockBuySell.java
@@ -11,12 +11,14 @@ public class StockBuySell {
         Mindp[0]=prices[0];
         
         for(int i=1;i<prices.length;i++){
+//maintain the lowest prices of the stock as a buying price.
             Mindp[i]=Math.min(Mindp[i-1],prices[i]);
 			
         //    Mindp[i-1]=Mindp[i];
         //    System.out.println("No "+i+" min buying price "+Mindp[i]);
+//current benefit we can get if we selling at the i day.
             Maxdp[i]=prices[i]-Mindp[i];
-            
+//update the global max profit;            
             reg=Math.max(reg,Maxdp[i]);
             
         }
@@ -24,7 +26,7 @@ public class StockBuySell {
     }
 	public static void main(String args[])
 	{
-		int[] arr = {3, 8, 8, 55, 38, 1, 7, 42, 54};
+		int[] arr = {3, 8, 8, 55, 38, 1, 7, 42, 54, 53};
 		StockBuySell sb = new StockBuySell();
 		int reg = sb.maxProfit(arr);
 		System.out.println("the output is " + reg);
